aaa5748e2e88ea878bcfc78f971f4378ae0d1451
Loïc Giraudel <lgiraudel@purch.com>
2015-11-30 00:03:21 +0100

    Redux devTools

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import App from './containers/App.jsx';
 import bddApp from './reducers/reducers';
 
 const loggerMiddleware = createLogger();
-const createStoreWithMiddleware = applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware
+const createStoreWithMiddleware = compose(
+    applyMiddleware(thunkMiddleware, loggerMiddleware),
+    devTools(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
 
 let store = createStoreWithMiddleware(bddApp);
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <div className='row'>
+        <div className='col-sm-9'>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </div>
+        <div className='col-sm-3'>
+            <DebugPanel rigth top bottom>
+                <DevTools store={store} monitor={LogMonitor}></DevTools>
+            </DebugPanel>
+        </div>
+    </div>,
     document.getElementById('react-container')
 );
